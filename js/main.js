112aa181694284d8940b7d97e57b7c7c9690a940
Yuriks1 <juris.adijans@iths.se>
2022-08-30 10:29:35 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//change nav style on scroll
+
+window.addEventListener('scroll',()=>
+    {
+        document.querySelector('nav')
+            .classList.toggle('window-scrolled', window.scrollY>0)
+    }
+
+)
