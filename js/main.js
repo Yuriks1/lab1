58a665f2730cdb52d2dafb9622df18d98ff1cf94
Yuriks1 <juris.adijans@iths.se>
2022-08-31 11:29:41 +0200

    Add picture.html

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ window.addEventListener('scroll',()=>
     }
 
 )
+
+//SwiperJS
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        599:{
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1023:{
+            slidesPerView: 3,
+            spaceBetween: 60,
+        }
+    }
+});
