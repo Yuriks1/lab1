1b6b796acf1cd40f6ad4cc65233534f21a436dfb
Yuriks1 <juris.adijans@iths.se>
2022-09-01 17:37:30 +0200

    Improve signIn.html

--- js/main.js
+++ js/main.js
@@ -66,16 +66,4 @@ async function renderUser() {
 renderUser();
 
 
-const email = document.getElementById("mail");
-
-email.addEventListener("input", (event) => {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("I am expecting an e-mail address!");
-        email.reportValidity();
-    } else {
-        email.setCustomValidity("");
-    }
-});
-
-
 
