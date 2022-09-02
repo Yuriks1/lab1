6903a6d25b7106bd96d7c32510904b7b714d0947
Yuriks1 <juris.adijans@iths.se>
2022-09-02 14:32:05 +0200

    Improve compability for tablet

--- js/main.js
+++ js/main.js
@@ -8,6 +8,49 @@ window.addEventListener('scroll',()=>
 
 )
 
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+const openNav = () =>{
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none'
+    closeNavBtn.style.display = 'inline-block'
+}
+openNavBtn.addEventListener('click',openNav);
+
+const closeNav = () =>{
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block'
+    closeNavBtn.style.display = 'none'
+}
+
+closeNavBtn.addEventListener('click',closeNav);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //SwiperJS
 
 var swiper = new Swiper(".mySwiper", {
@@ -64,6 +107,9 @@ async function renderUser() {
 }
 
 renderUser();
+
+
+
 
 
 
