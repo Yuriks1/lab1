aba55131139edbcf25660a5b8dafb9d0b80564eb
Yuriks1 <juris.adijans@iths.se>
2022-09-04 12:13:51 +0200

    Reformat code

--- js/main.js
+++ js/main.js
@@ -147,3 +147,28 @@ let saveFile = () => {
     }
     newLink.click();
 }
+
+
+
+
+
+
+
+
+/*============Save as PDF================*/
+
+function generatePDF() {
+
+    var element = document.getElementById('suggestion_box');
+    element.style.width = '400px';
+    element.style.height = '600px';
+    var opt = {
+        margin:       0.5,
+        filename:     'myfile.pdf',
+        image:        { type: 'jpeg', quality: 1 },
+        html2canvas:  { scale: 1 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait',precision: '12' }
+    };
+
+    html2pdf().set(opt).from(element).save();
+}
